229da670a720928f8fce28b9e8dd91846b0bc682
Felicia <felicia.saar@gmx.de>
2016-07-22 00:57:15 +0200

    cleanup, todos left

--- simple-plot-transferfunction-date.py
+++ simple-plot-transferfunction-date.py
@@ -1,30 +1,94 @@
+#TODO description
+'''
+Script to analyze ...
+authors: Eric, Adrian, Tjasa, Felicia
+'''
+
+
+###############################################################################
+## imports
+###############################################################################
+
 import numpy as np
 import matplotlib.pyplot as plt
 import os
 import glob
+# get DataLoader from https://github.com/relacs/pyrelacs
 import DataLoader as dl
 
-#os.chdir("/home/felicia/Dokumente/01 - Uni/01-SS16/Sensory Systems in Natural Environments/Data/DistanceMeadow/")
+###############################################################################
+## functions
+###############################################################################
 
-aa = np.loadtxt(fname='2016-07-21-aa-meadow/transferfunction-data.dat')
-
-c = 1
-for dir in sorted(glob.glob('2016-07-21-*-meadow')) :
-    info = dl.load(dir + '/info.dat')[0]
+def get_distance(directory) :
+    '''returns the distance in m'''
+    file = directory + '/info.dat'
+    info = dl.load(file)[0]
     distancestr = info['Distance']
     distance = 0.01*float(distancestr.replace('cm',''))
-    file = dir + '/transferfunction-data.dat'
-    dat = np.loadtxt(fname=file)
-    ax = plt.subplot(3,4,c)
+    return distance
+
+def gain_vs_frequency(filename) :
+    dat = np.loadtxt(fname=filename)
     x = dat[:,0]
-    y = dat[:,1]/aa[:,1]
-    ytdist = distance*y
-    ytdistlog = np.log10(ytdist)
-    ax.plot(x,ytdistlog)
+    y = dat[:,1]
+    ylabel = "gain"
+    return x, y, ylabel
+
+def exclude_pure_distance_effects(gain, distance) :
+    ''' eliminates effects due to the general decrease of sound pressure with the distance
+    -- https://en.wikipedia.org/wiki/Inverse-square_law#Acoustics
+    -- sound pressure ~ 1/distance
+    '''
+    g = distance*gain
+    ylabel = "gain*distance" #TODO find better name
+    return g, ylabel
+
+###############################################################################
+## main script
+###############################################################################
+
+# set working directory
+#os.chdir("/home/felicia/Dokumente/01 - Uni/01-SS16/Sensory Systems in Natural Environments/Data/DistanceMeadow/")
+
+# get reference recordings (distance 100cm)
+_ , reference_gain, _ = gain_vs_frequency('2016-07-21-aa-meadow/transferfunction-data.dat')
+
+counter = 1
+for dir in sorted(glob.glob('2016-07-21-*-meadow')) :
+
+    # get path and general information
+    file = dir + '/transferfunction-data.dat'
+    distance = get_distance(dir)
+
+    # construct subplot structure
+    ax = plt.subplot(3,4,counter) #TODO adapt
+
+    # values for frequency and gain (untransformed)
+    freq, gain, ylabel = gain_vs_frequency(file)
+    # normalize gain
+    gain = gain/reference_gain
+    # eliminate pure distance effects
+    gain, ylabel = exclude_pure_distance_effects(gain,distance)
+    # go to logarithmic scale
+    gain = np.log10(gain)
+    ylabel = "log " + ylabel
+    # set the title
+    title = dir #TODO title finding
+
+    # plot
+    ax.plot(freq, gain)
+    ax.set_title(title)
     ax.set_xlabel("frequency [Hz]")
-    ax.set_ylabel("gain")
-    ax.set_xlim(2000, 20000)
+    ax.set_ylabel(ylabel)
+    # adapt the limits
+    ax.set_xlim(5000, 20000)
     ax.set_ylim(-3,0)
-    ax.set_title(dir)
-    c = c + 1
+
+    # counter needed for handle the subplots
+    counter = counter + 1
+
+# show all the plots
 plt.show()
+
+#TODO : title, several graphs in one plot, beautify, rename
